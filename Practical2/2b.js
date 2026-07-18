718c50853a97df572dc338f0f401c2ce4eb82b2f
suyashkarnalemvlu-art <suyashkarnalemvlu@gmail.com>
2026-07-18 11:54:56 +0530

    Create 2b.js

---
+++ Practical2/2b.js
@@ -0,0 +1,11 @@
+let fruits = ["Mango","Banana","Apple"];
+let student = {
+name: "Suyash",
+age: 19,
+city: "Mumbai"
+};
+console.log("Fruits :",fruits);
+console.log("Second Fruits :",fruits[1]);
+console.log("Student Name:", student.name);
+console.log("Student Age: ",student.age);
+console.log("Student City:", student.city);
