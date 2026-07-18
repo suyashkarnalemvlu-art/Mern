276a08fd65311e0d2095b0c9fe1a9c33a3e522c4
suyashkarnalemvlu-art <suyashkarnalemvlu@gmail.com>
2026-07-18 11:51:32 +0530

    Add basic arithmetic functions and logging

---
+++ Practical1/1d.js
@@ -0,0 +1,13 @@
+let num = 15;
+let num2 = 10;
+function Add(a,b)
+{
+return a+b;
+}
+function Sub(a,b)
+{
+return a-b;
+}
+console.log("Addition = ",Add(num,num2));
+console.log("Subtraction = ",Sub(num,num2));
+console.log("Suyash Karnale T012")
